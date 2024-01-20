b6eb135d520a29e33c503a0227836f738d337a72
Sourav0809 <spathak7431@gmail.com>
2024-01-20 12:28:59 +0530

    path fixed

--- src/pages/ExpensePage.jsx
+++ src/pages/ExpensePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AddExpenseForm from "../components/expenses/ui/AddExpenseForm";
-import ExpenseContainer from "../components/expenses/expenseContainer";
+import ExpenseContainer from "../components/expenses/ExpenseContainer";
 
 const ExpensePage = () => {
   return (
